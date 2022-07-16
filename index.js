511bb420433623e2ca093e59ce3c0f5350391067
Christopher DiStasio <cadistasio@gmail.com>
2022-07-16 12:38:10 -0500

    Adds places controller

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require("express");
 const app = express();
 
+app.use('/places', require('./controllers/places'));
+
 app.get('/', (req, res) => {
 	res.send("Hello there");
 })
@@ -9,6 +11,5 @@ app.get('/', (req, res) => {
 app.get("*", (req, res) => {
 	res.status(404).send('<h1>404 Page</h1>')
 })
-
 
 app.listen(process.env.PORT);
